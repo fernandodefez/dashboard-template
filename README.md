b3f9aeb2269184b2e6d53e8faa51cef6e4c27583
fernandodefez <fernandodefez@outlook.com>
2022-05-27 03:03:18 -0500

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+## Dashboard Template
+
+This dashboard was built by using Bootstrap v.5.0.2
+
+This template has the following basic pages:
+
+- Landing Page
+- Register
+- Login
+- Dashboard
+
+
+If you would like to take a look at the template click the link below
---
+++ assets/js/sidebar.js
@@ -0,0 +1,9 @@
+function openSideBar() {
+    document.getElementById('SideBarOverlay').style.display = 'block';
+    document.getElementById('MobileSideBar').style.display = 'block';
+}
+
+function closeSideBar() {
+    document.getElementById('SideBarOverlay').style.display = 'none';
+    document.getElementById('MobileSideBar').style.display = 'none';
+}
